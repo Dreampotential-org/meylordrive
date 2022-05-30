4aea2142449cb34beba11333f2ab3a0cebec9b37
Aaron Rosen <aaronorosen@gmail.com>
2022-05-30 10:30:19 -0700

    add some more

--- codes/agent/management/commands/run_tasks.py
+++ codes/agent/management/commands/run_tasks.py
@@ -1,10 +1,23 @@
 from django.core.management.base import BaseCommand
+from paramiko.client import SSHClient
 
 from tasks.models import Task, Server
 
 
 def run_job(server, task):
-    print("Run job server: %s %s" % (task, server))
+    print("Run job server: %s %s" % (server.username, server.ip_address))
+    client = SSHClient()
+    client.load_system_host_keys()
+    client.connect(server.ip_address, username=server.username)
+    stdin, stdout, stderr = client.exec_command('ls -l')
+
+    out = stdout.read().decode().strip()
+    error = stderr.read().decode().strip()
+    if error:
+        raise Exception('There was an error pulling the runtime: {}'.format(error))
+    print(out)
+    client.close()
+
 
 
 class Command(BaseCommand):
@@ -21,5 +34,8 @@ class Command(BaseCommand):
 
         for task in tasks:
             print(task)
-            server = Server.objects.filter(active=False).first()
+            server = Server.objects.filter().first()
             run_job(server, task)
+        servers = Server.objects.filter()
+        for server in servers:
+            run_job(server, None)
