75abb0b490a3a3a4db8694bcf95fd0c02f575639
Aaron Rosen <aaronorosen@gmail.com>
2022-05-31 16:28:03 -0700

    add parallel-ssh example

--- codes/agent/management/commands/run_tasks.py
+++ codes/agent/management/commands/run_tasks.py
@@ -1,6 +1,10 @@
 from django.core.management.base import BaseCommand
 from paramiko.client import SSHClient, AutoAddPolicy
 import threading
+from pssh.clients import ParallelSSHClient
+
+from pssh.config import HostConfig
+
 
 from tasks.models import Task, Server
 
@@ -10,7 +14,8 @@ def run_job(server, task):
     client.load_system_host_keys()
     client.set_missing_host_key_policy(AutoAddPolicy())
     client.connect(server.ip_address, username=server.username)
-    stdin, stdout, stderr = client.exec_command('git clone git@github.com:aaronorosen/django-zillow.git')
+    # stdin, stdout, stderr = client.exec_command('git clone git@github.com:aaronorosen/django-zillow.git')
+    stdin, stdout, stderr = client.exec_command('sudo apt-get update')
 
     task.stdout = stdout.read().decode().strip()
     task.stderr = stderr.read().decode().strip()
@@ -34,6 +39,29 @@ class Command(BaseCommand):
         print("Number of tasks to run: %s" % len(tasks))
 
         threads = []
+        hosts = []
+        host_config = []
+        servers = Server.objects.filter()
+        for server in servers:
+            hosts.append(server.ip_address)
+            host_config.append(
+                HostConfig(port=22, user='aaronoro',
+                            private_key='server-key'),
+                )
+
+        client = ParallelSSHClient(hosts, host_config=host_config)
+
+        output = client.run_command('cd ~; git clone git@github.com:aaronorosen/django-zillow.git')
+        output = client.run_command('uname')
+        client.join()
+
+        for host_output in output:
+            for line in host_output.stdout:
+                print(line)
+            exit_code = host_output.exit_code
+            print("Error exit code: %s" % exit_code)
+
+        return
 
         for task in tasks:
             print(task)
@@ -41,7 +69,6 @@ class Command(BaseCommand):
             t = threading.Thread(target=run_job, args=(server, task))
             t.start()
             threads.append(t)
-        servers = Server.objects.filter()
         for server in servers:
             task = Task()
             t = threading.Thread(target=run_job, args=(server, task))
