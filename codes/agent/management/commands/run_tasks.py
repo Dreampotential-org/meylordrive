cfd6fd60fc3594919fcda060a5c383c6e9a52999
John Meylor <leomeylor@gmail.com>
2022-06-02 16:35:03 -0700

    apt-get update -y works

--- codes/agent/management/commands/run_tasks.py
+++ codes/agent/management/commands/run_tasks.py
@@ -13,14 +13,20 @@ def run_job(server, task):
     client.connect(server.ip_address, username=server.username,allow_agent=True) 
     #print(pprint.pprint(dir(client)))
     #client.channel.request_forward_agent()
-    stdin, stdout, stderr = client.exec_command('ls -alh')
+    #stdin, stdout, stderr = client.exec_command('ls -alh /etc/apt/sources.list.d/')
+    #stdin, stdout, stderr = client.exec_command('sudo rm -f /etc/apt/sources.list.d/buildkite-agent.list.save ; sudo cp /var/lib/dpkg/status-old /var/lib/dpkg/status ; sudo apt-get update -y')
+    #stdin, stdout, stderr = client.exec_command('sudo cp /var/lib/dpkg/status-old /var/lib/dpkg/status')
+    #stdin, stdout, stderr = client.exec_command('sudo mkdir /var/lib/dpkg ; sudo touch /var/lib/dpkg/status ; sudo apt-get update -y')
+    stdin, stdout, stderr = client.exec_command('sudo apt-get update -y')
 
     task.stdout = stdout.read().decode().strip()
     task.stderr = stderr.read().decode().strip()
     task.save()
 
     if task.stderr != "":
-        raise Exception('There was an error pulling the runtime: {}'.format(task.stderr))
+        print("%s@%s" % (server.username, server.ip_address))
+        print(task.stderr)
+        #raise Exception('There was an error pulling the runtime: {}'.format(task.stderr))
     print(task.stdout)
     client.close()
 
