e344dd5f61a9c2d2a0255e135cb08cd8de804f35
you@gmail.com <you@gmail.com>
2022-06-11 15:06:55 +0530

    postAPIaadedForTasksModel

--- codes/web/settings.py
+++ codes/web/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tasks',
     'agent',
+    'rest_framework',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -49,8 +52,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'web.urls'
 
 TEMPLATES = [
@@ -87,7 +93,6 @@ DATABASES = {
         'PASSWORD': db_password,
     }
 }
-
 
 
 # Password validation
