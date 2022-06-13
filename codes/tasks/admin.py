dc4deeafea88ac25f8e0e0ec5f15ec2bfc1b7545
you@gmail.com <you@gmail.com>
2022-06-14 00:28:55 +0530

    githubHooksAminRegister

--- codes/tasks/admin.py
+++ codes/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from tasks.models import GithubHook
 
-# Register your models here.
+
+class GitHubAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(GithubHook, GitHubAdmin)
