cba7df3a937f2032dc989529463a5cc0008967d7
Aaron Rosen <aaronorosen@gmail.com>
2022-06-14 14:05:06 -0700

    add more

--- codes/tasks/models.py
+++ codes/tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
     unique = True
-    status = models.CharField(max_length=64)
+    status = models.CharField(max_length=64, blank=True, null=True)
     command = models.CharField(max_length=4096, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
