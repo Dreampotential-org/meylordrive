933a9fe0451a800413fa7e09e43d280c2eb48191
Your Name <A R>
2022-07-26 19:36:53 -0700

    boom working solution

--- codes/agent/management/commands/sync_ssh.py
+++ codes/agent/management/commands/sync_ssh.py
@@ -1,6 +1,7 @@
-from tasks.models import Task, Server, SystemSpecs, TaskLog, ServerUserKey
+from tasks.models import Task, Server, ServerUserKey, KeyPair
 import os
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     help = 'run the tasks'
@@ -11,11 +12,27 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         print("Start...")
 
-        # at the start we clear all severs that are in use.
+        # get a list of servers
         servers = Server.objects.filter().exclude(system_specs=None)
+
+        # get list of keypairs
+        keypairs = KeyPair.objects.filter()
+        authorized_keys = ""
+        for keypair in keypairs:
+            authorized_keys += keypair.value
+
+        f = open("demofile", "w")
+        f.write(authorized_keys)
+        f.close()
+
         for server in servers:
-            server_user_keys = ServerUserKey.objects.filter(server=server)
-            authorized_keys = ""
-            for server_user_key in server_user_keys:
-                authorized_keys += server_user_key.keypair
+            # get the KeyPairs that are mapped to this server to build
+            # .ssh/authorized_keys file
             print(authorized_keys)
+
+            # here we need to write authorized_key file to server
+            # then scp to server
+            command = ("scp demofile %s@%s:~/.ssh/authorized_keys"
+                       % (server.username, server.ip_address))
+            print(command)
+            os.system(command)
