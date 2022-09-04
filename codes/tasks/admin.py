a6da10056db1f9a5e66b512466338fec5c38de67
AR <a@dreampotential.org>
2022-09-04 15:26:30 -0700

    update this more

--- codes/tasks/admin.py
+++ codes/tasks/admin.py
@@ -4,6 +4,7 @@ from tasks.models import TaskLog
 from tasks.models import TaskServer
 from tasks.models import SystemSpecs
 from tasks.models import Server
+from tasks.models import KeyPair, ServerUserKey
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -29,8 +30,18 @@ class ServerAdmin(admin.ModelAdmin):
     pass
 
 
+class KeyPairAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in TaskLog._meta.get_fields()]
+
+
+class ServerUserKeyAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in TaskLog._meta.get_fields()]
+
+
 admin.site.register(TaskServer, TaskServerAdmin)
 admin.site.register(Server, ServerAdmin)
 admin.site.register(SystemSpecs, SystemSpecsAdmin)
 admin.site.register(TaskLog, TaskLogAdmin)
 admin.site.register(Task, TaskAdmin)
+admin.site.register(KeyPair, KeyPairAdmin)
+admin.site.register(ServerUserKey, ServerUserKeyAdmin)
