e0b42d523c4c58b16a0d39b62357e3e3c5bcdb6c
Ubuntu <ubuntu@ip-172-31-40-199.us-east-2.compute.internal>
2022-09-15 23:04:23 +0000

    fix the key import

--- codes/tasks/apps.py
+++ codes/tasks/apps.py
@@ -1,5 +1,17 @@
+import os
 from django.apps import AppConfig
+import time
 
 
 class TasksConfig(AppConfig):
     name = 'tasks'
+    verbose_name = "My Application"
+
+    def ready(self):
+        os.system("eval `ssh-agent -s`")
+        os.system("chmod 600 /opt/server-key")
+        time.sleep(5)
+        os.system("ssh-add")
+        os.system("ssh-add /opt/server-key")
+
+
