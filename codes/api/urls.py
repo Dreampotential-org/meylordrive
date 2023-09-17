54213463386f543530f70ab1abd49aaf4dc06f59
aaronorosen@gmail.com <Aaron Rosen>
2023-09-17 00:59:32 -0700

    fix route

--- codes/api/urls.py
+++ codes/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/api/create-org', views.create_org),
-    path('/api/list-orgs', views.list_orgs),
+    path('api/create-org', views.create_org),
+    path('api/list-orgs', views.list_orgs),
 
 ]
