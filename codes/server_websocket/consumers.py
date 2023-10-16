fb60c4c6b06957d567ad8b92c9fbee84adcdbd85
aaronorosen@gmail.com <Aaron Rosen>
2023-10-16 13:48:46 -0700

    update this program some more

--- codes/server_websocket/consumers.py
+++ codes/server_websocket/consumers.py
@@ -6,6 +6,9 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from django.db import transaction
 from asgiref.sync import async_to_sync
+from tasks.models import StatsEntry
+from tasks.models import ProjectCommand
+from utils.chirp import CHIRP
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -14,67 +17,62 @@ class ChatConsumer(AsyncWebsocketConsumer):
     """
 
     async def connect(self):
-        """
-        Async function called when the WebSocket is handshaking as part of connection process.
-        """
         await self.accept()
 
     async def disconnect(self, close_code):
-        """
-        Async function called when the WebSocket closes for any reason.
-        :param close_code: Provides the reason the WebSocket closed.
-        """
         pass
 
     async def receive(self, text_data):
-        """
-        Async function called when the server receives a message from WebSocket.
-        :param text_data: Text data received from the WebSocket.
-        """
-        stats_json = json.loads(text_data)  # Decoding the received JSON text data
-        # Calling create_entry async function to create a new entry in the database with the received data
-        new_entry = await self.create_entry(stats_json)
-        # Uncomment the next line if you want to print the newly created entry's data
+        # Decoding the received JSON text data
+        stats_json = json.loads(text_data)
+        # Calling create_entry async function to create a new entry
+        # in the database with the received data
+        new_entry = await self.save_stats_entry(stats_json)
+        CHIRP.info("New entry created: %s" % new_entry)
+        # Uncomment the next line if you want to print
+        # the newly created entry's data
         # await self.print_stats_data(new_entry)
+
+        # XXX Make it more obvious how this api is working
+        # /get_task
+
+        # XXX how to fetch Server stats-agent is active on
         await self.send_task_data()
 
     async def send_task_data(self):
         # Call the sync function to get project command in async context
-        project_command = await database_sync_to_async(self.get_project_command)()
+        project_command = await database_sync_to_async(
+            self.get_project_command
+        )()
 
         # Check if a project_command is found
         if project_command:
             print(f"Project Command: {project_command}")
             await self.send(json.dumps(project_command))
         else:
-            # If no project_command is found, you can send a message to indicate that the queue is empty
+            # If no project_command is found, you can send a message
+            # to indicate that the queue is empty
             await self.send(json.dumps({'message': 'No tasks available.'}))
 
     # Define synchronous method for getting project command
     def get_project_command(self):
-        from tasks.models import ProjectCommand
         with transaction.atomic():
 
-            project_command = ProjectCommand.objects.select_for_update(skip_locked=True).filter().exclude(status__isnull=False).first()
-            # project_command = ProjectCommand.objects.filter().values()
-            # project_command = ProjectCommand.objects.filter(status=None).first()
-            print(f"Project: {project_command}")
+            project_command = ProjectCommand.objects.select_for_update(
+                skip_locked=True
+            ).filter().exclude(status__isnull=False).first()
 
             if project_command:
+                # XXX We need some way to handle status when is not running
+                # or in error state
                 project_command.status = 'running'
                 project_command.save()
         return project_command
 
-    @database_sync_to_async  # Decorating the function to be run in a synchronous manner
-    def create_entry(self, stats_json):
-        """
-        Synchronous function to create a new entry in the database with the provided JSON data.
-        Decorated with @database_sync_to_async to be called from async contexts.
-        :param stats_json: The JSON data used to create a new entry.
-        :return: The newly created entry.
-        """
-        from tasks.models import StatsEntry
-        # Creating and returning a new StatsEntry with the data provided in stats_json
+    # Decorating the function to be run in a synchronous manner
+    @database_sync_to_async
+    def save_stats_entry(self, stats_json):
+
         return StatsEntry.objects.create(
             system=stats_json['System'],
             node_name=stats_json['Node Name'],
@@ -96,22 +94,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @staticmethod
     async def print_stats_data(entry):
-        """
-        Async static method to print the data of the provided entry.
-        :param entry: The entry whose data is to be printed.
-        """
-        # Retrieving and printing the fields of the provided entry using the get_fields static method
-        stats_data = await database_sync_to_async(ChatConsumer.get_fields)(entry)
+        stats_data = await database_sync_to_async(
+            ChatConsumer.get_fields
+        )(entry)
         print(stats_data)
 
     @staticmethod
     def get_fields(entry):
-        """
-        Static method to retrieve and return the fields of the provided entry as a dictionary.
-        :param entry: The entry whose fields are to be retrieved.
-        :return: A dictionary containing the fields of the entry.
-        """
-        # Creating and returning a dictionary with the fields and values of the provided entry
+        # XXX Expand to take more snapshot of node and processing table
         return {
             'system': entry.system,
             'node_name': entry.node_name,
