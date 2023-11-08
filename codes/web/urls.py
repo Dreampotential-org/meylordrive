d127fa2578e562b9001ed3a70716ec83ffdacf26
Nadeem <shaiknadeem0591@gmail.com>
2023-11-09 02:32:42 +0530

    update the code more

--- codes/web/urls.py
+++ codes/web/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from codes.authentication import views
+from authentication import views
 from django.urls import re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
