6bfd97f30effd549f0107090d6ce1a5eb3467b6e
AR <aaronorosen@gmail.com>
2023-11-22 07:31:13 +0000

    start add models for calls and contacts

--- codes/drive/models.py
+++ codes/drive/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Phone(models.Model):
+    number = models.CharField(max_length=20, unique=True)
+
+
+class SMS(models.Model):
+    phone = models.ForeignKey(to=Phone, on_delete=models.CASCADE,
+                                     default="")
+    number = models.CharField(max_length=20, blank=True, null=True)
+    msg = models.CharField(max_length=500, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Call(models.Model):
+    phone_number = models.ForeignKey(to=Phone, on_delete=models.CASCADE,
+                                     default="")
+    number = models.CharField(max_length=20,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    time_duration = models.IntegerField(default=0)
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=100, blank=True, null=True)
+    owner_name = models.CharField(max_length=100, blank=True, null=True)
+    owner_phone = models.CharField(max_length=100, blank=True, null=True)
+    url = models.CharField(max_length=100, blank=True, null=True)
+    state = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    price = models.CharField(max_length=100, blank=True, null=True)
