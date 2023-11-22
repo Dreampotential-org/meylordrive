dbcf8b43e8b81646c3db8931ff119fc010a06753
aaronorosen@gmail.com <Aaron Rosen>
2023-11-21 16:54:15 -0800

    Add transcribe video example

---
+++ codes/drive/management/commands/transcribe_video.py
@@ -0,0 +1,85 @@
+import subprocess, sys, os, json
+
+from datetime import datetime, timedelta
+
+from vosk import Model, KaldiRecognizer
+
+SAMPLE_RATE = 16000
+CHUNK_SIZE = 4000
+
+
+class Transcriber():
+    def __init__(self, model_path):
+        self.model = Model(model_path)
+
+    def fmt(self, data):
+        data = json.loads(data)
+
+        start = min(r["start"] for r in data.get("result", [{ "start": 0 }]))
+        end = max(r["end"] for r in data.get("result", [{ "end": 0 }]))
+
+        return {
+            "start": str(timedelta(seconds=start)),
+            "end": str(timedelta(seconds=end)),
+            "text": data["text"]
+        }
+
+    def transcribe(self, filename):
+        rec = KaldiRecognizer(self.model, SAMPLE_RATE)
+        rec.SetWords(True)
+
+        if not os.path.exists(filename):
+            raise FileNotFoundError(filename)
+
+        transcription = []
+
+        ffmpeg_command = [
+                "ffmpeg",
+                "-nostdin",
+                "-loglevel",
+                "quiet",
+                "-i",
+                filename,
+                "-ar",
+                str(SAMPLE_RATE),
+                "-ac",
+                "1",
+                "-f",
+                "s16le",
+                "-",
+            ]
+
+        with subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE) as process:
+
+            start_time = datetime.now() 
+            while True:
+                data = process.stdout.read(4000)
+                if len(data) == 0:
+                    break
+
+                if rec.AcceptWaveform(data):
+                    transcription.append(self.fmt(rec.Result()))
+
+            transcription.append(self.fmt(rec.FinalResult()))
+            end_time = datetime.now()
+
+            time_elapsed = end_time - start_time
+            print(f"Time elapsed  {time_elapsed}")
+
+        return {
+            "start_time": start_time.isoformat(),
+            "end_time": end_time.isoformat(),
+            "elapsed_time": time_elapsed,
+            "transcription": transcription,
+        }
+
+import pprint
+
+filename = "AgentStat.m4a"
+model_path = "vosk-model-small-en-in-0.4"
+
+transcriber = Transcriber(model_path)
+transcription = transcriber.transcribe(filename)
+
+pprint.pprint(transcription)
+
