fec2d3baecdc5a717beb6785be19f1a0c54b90bf
Nadeem <shaiknadeem0591@gmail.com>
2023-11-24 14:49:52 +0530

    deleted unccesary apps

--- codes/chat_websocket/urls.py
+++
@@ -1,9 +0,0 @@
-# chat_websocket/routing.py
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/chat/room/(?P<room_name>\w+)/$", consumers.ChatRoomConsumer.as_asgi()),
-    # re_path(r"ws/chat/room_list/$", consumers.RoomListConsumer.as_asgi()),
-    # Add URLs for other room-related functionalities
-]
