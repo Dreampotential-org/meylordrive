99b0cff8941e6d919ca6f5cdcf9f0c87d946e3dc
aaronorosen@gmail.com <Aaron Rosen>
2023-11-28 10:37:35 -0800

    add initial manage_linkedin script

---
+++ codes/drive/management/commands/manage_linkedin.py
@@ -0,0 +1,33 @@
+import time
+import os
+from django.core.management.base import BaseCommand
+
+from utils.browser import init_driver
+
+
+
+class Command(BaseCommand):
+    help = 'Import address extra'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        print("here is the start")
+        driver = init_driver("firefox")
+        driver.get('https://linkedin.com/')
+
+        # login with 
+        # realtorstat@gmail.com
+        # AgentStat123!
+
+        # login meeting
+        # parse events in meeting
+        # while True:
+        #    parse_event = driver.find_element()..
+        #    if parse_event == 'join':
+        #    from playsound import playsound
+        #    playsound("breath.wav")
+        #    if parse_event == 'leave':
+
+
