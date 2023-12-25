27aacfa7b19b9929ce9b5eab6bb28935cd7226b9
Nadeem <shaiknadeem0591@gmail.com>
2023-12-25 15:23:54 +0530

    this will let us to    cretae the room as per the query

--- codes/web/routing.py
+++ codes/web/routing.py
@@ -3,6 +3,6 @@ from .consumers import ChatConsumer
 
 
 websocket_urlpatterns = [
-    path("ws/contact/<room_slug>/", ChatConsumer.as_asgi()),
-    re_path(r'^ws/contact/$', ChatConsumer.as_asgi()),
+    re_path(r'^ws/(?P<room_slug>[\w-]+)/$', ChatConsumer.as_asgi()),
+    # re_path(r'^ws/contact/$', ChatConsumer.as_asgi()),
 ]
