b283c4e47946acefc1fa3a907b03893c80e3ed30
Nadeem <shaiknadeem0591@gmail.com>
2023-12-31 00:09:53 +0530

    send_message fucntion wasworking

--- codes/web/routing.py
+++ codes/web/routing.py
@@ -1,8 +1,10 @@
 from django.urls import path , include,re_path
-from web.consumers import ChatConsumer
+from .consumers import ChatConsumer
 
 
 websocket_urlpatterns = [
     path("ws/contact/<room_slug>/", ChatConsumer.as_asgi()),
     re_path(r'^ws/contact/$', ChatConsumer.as_asgi()),
+    path('ws/contact/<str:room_slug>/', ChatConsumer.as_asgi()),
+
 ]
