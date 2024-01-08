19b7e35d63a861f51043e9fad3f90ed0acfa3acd
Ben viP <bengg2040@gmail.com>
2024-01-08 15:17:33 -0800

    add devices api

--- codes/ashe/urls.py
+++ codes/ashe/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('start', views.start, name="start"),
     path('stop', views.stop, name="stop"),
+    path('devices', views.devices, name="devices"),
     path('bulk_sync_motions', views.bulk_sync_motions, name="bulk_sync_motions"),
     path('session_point', views.session_point,
          name="session_point"),
@@ -15,6 +16,5 @@ urlpatterns = [
     path('stats', views.get_session_stats, name="get_session_stats"),
     path('gsm_Add', views.gsm_Add, name="gsm_Add"),
     path('gsm_send', views.gsm_send, name="gsm_send"),
-    
 
 ]
