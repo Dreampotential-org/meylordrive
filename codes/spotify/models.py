c3bc67695d379790abc10332ce130d74cb7b6b45
Ben viP <bengg2040@gmail.com>
2024-01-29 13:01:28 -0800

    add this updat

--- codes/spotify/models.py
+++ codes/spotify/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
+class SogAub(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.TextField()
 
 class Sog(models.Model):
     name = models.TextField()
     seconds = models.IntegerField(default=0)
     sog_group = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE,
+        SogAub, on_delete=models.CASCADE,
         null=True, blank=True, default=None
     )
 
-
-class SogAub(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
-                             null=True, blank=True, default=None)
 
