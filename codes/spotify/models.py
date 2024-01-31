3fcdf0ebc2aa6cee1996ccd0ef6bd1d6b993878e
Nadeem <shaiknadeem0591@gmail.com>
2024-02-01 00:04:15 +0530

    added for both spotify and yt  and requireemtn.txt upadted

--- codes/spotify/models.py
+++ codes/spotify/models.py
@@ -16,3 +16,24 @@ class SogAub(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
                              null=True, blank=True, default=None)
 
+
+
+class Song(models.Model):
+    artist_name = models.CharField(max_length=255)
+    track_title = models.CharField(max_length=255)
+    track_number = models.IntegerField()
+    album_name = models.CharField(max_length=255)
+    release_date = models.DateField()
+    isrc = models.CharField(max_length=20, blank=True, null=True)
+    album_art = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.artist_name} - {self.track_title}"
+
+class DownloadedSong(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    download_date = models.DateTimeField(auto_now_add=True)
+    file_path = models.FileField(upload_to="downloaded_songs/")
+
+    def __str__(self):
+        return f"{self.song.artist_name} - {self.song.track_title} - {self.download_date}"
