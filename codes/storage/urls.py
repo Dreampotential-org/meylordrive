b612327e990a71bfa63f2a26732a2e6aa2fac68a
nadeem-0591 <85397391+nadeem-0591@users.noreply.github.com>
2024-02-07 11:34:22 +0530

    added url for the view fucntion of listing dowloaded songs in storage app urls.py

--- codes/storage/urls.py
+++ codes/storage/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('list_comments/<int:upload_id>',
          views.list_comments, name='list_comments'),
     path('get_media/', views.get_media, name="get_media"),
+    path('downloaded-songs/', views.list_downloaded_songs, name='list_downloaded_songs'),
+
     path('stream_media', views.stream_video, name="stream_video"),
 ]
